ebe585ce656edcb985211026778ed7c3f2c739e7
OuYang <ouyangxiongbin@gold-gold.cn>
2016-10-28 17:30:08 +0800

    增加swift版本

---
+++ OYCountDown(Swift)/OYCountDown/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  OYCountDown
+//
+//  Created by Gold on 2016/10/27.
+//  Copyright © 2016年 Gold. All rights reserved.
+//
+
+import UIKit
+
+class Model: NSObject {
+    var timer: String?
+    var title: String = ""
+
+}
